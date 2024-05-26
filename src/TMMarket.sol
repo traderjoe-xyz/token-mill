098894b715bb29699bafe5346597aac65e786f24
0x0Louis <louis@joepegs.com>
2024-05-26 21:11:39 +0200

    Update TMMarket

--- src/TMMarket.sol
+++ src/TMMarket.sol
@@ -14,7 +14,8 @@ contract TMMarket is PricePoints, ImmutableContract, ITMMarket {
     using SafeERC20 for IERC20;
 
     bool internal _locked;
-    uint128 internal _circulatingSupply;
+    uint128 internal _baseReserve;
+    uint256 internal _quoteReserve;
 
     uint256 internal _protocolClaimedFees;
     uint256 internal _protocolTotalFees;
@@ -29,6 +30,12 @@ contract TMMarket is PricePoints, ImmutableContract, ITMMarket {
         _locked = false;
     }
 
+    function initialize() external override {
+        if (msg.sender != _factory()) revert Market__OnlyFactory();
+
+        _baseReserve = uint128(_totalSupply());
+    }
+
     function getBaseToken() external pure override returns (address) {
         return _baseToken();
     }
@@ -38,17 +45,17 @@ contract TMMarket is PricePoints, ImmutableContract, ITMMarket {
     }
 
     function getCirculatingSupply() external view override returns (uint256) {
-        return _circulatingSupply;
+        return _totalSupply() - _baseReserve;
     }
 
     function getTotalSupply() external pure override returns (uint256) {
         return _totalSupply();
     }
 
-    function getPriceAt(uint256 circulatingSupply, bool bid) external pure override returns (uint256) {
+    function getPriceAt(uint256 circulatingSupply, bool fillBid) external pure override returns (uint256) {
         uint256 totalSupply = _totalSupply();
 
-        if (circulatingSupply >= totalSupply) return _pricePoints(_pricePointsLength() - 1, bid);
+        if (circulatingSupply >= totalSupply) return _pricePoints(_pricePointsLength() - 1, fillBid);
 
         circulatingSupply = circulatingSupply * 1e18 / _basePrecision();
         uint256 widthScaled = _widthScaled();
@@ -56,32 +63,45 @@ contract TMMarket is PricePoints, ImmutableContract, ITMMarket {
         uint256 i = circulatingSupply / widthScaled;
         uint256 supply = circulatingSupply % widthScaled;
 
-        uint256 p0 = _pricePoints(i, bid);
-        uint256 p1 = _pricePoints(i + 1, bid);
+        uint256 p0 = _pricePoints(i, fillBid);
+        uint256 p1 = _pricePoints(i + 1, fillBid);
 
         return p0 + (p1 - p0) * supply / widthScaled;
     }
 
-    function getPricePoints(bool bid) external pure override returns (uint256[] memory) {
+    function getPricePoints(bool fillBid) external pure override returns (uint256[] memory) {
         uint256 length = _pricePointsLength();
 
         uint256[] memory prices = new uint256[](length);
 
         for (uint256 i; i < length; ++i) {
-            prices[i] = _pricePoints(i, bid);
+            prices[i] = _pricePoints(i, fillBid);
         }
 
         return prices;
     }
 
     function getPendingFees() external view override returns (uint256 protocolFees, uint256 creatorFees) {
-        (uint256 pendingProtocolFees, uint256 pendingCreatorFees) =
+        (, uint256 pendingProtocolFees, uint256 pendingCreatorFees) =
             _getPendingFees(ITMFactory(_factory()), IERC20(_quoteToken()));
 
         return (
             _protocolTotalFees + pendingProtocolFees - _protocolClaimedFees,
             _creatorTotalFees + pendingCreatorFees - _creatorClaimedFees
         );
+    }
+
+    function getDeltaAmounts(int256 deltaAmount, bool fillBid)
+        external
+        view
+        override
+        returns (int256 deltaBaseAmount, int256 deltaQuoteAmount)
+    {
+        uint256 circulatingSupply = _totalSupply() - _baseReserve;
+
+        return (deltaAmount > 0) == fillBid
+            ? getDeltaQuoteAmount(circulatingSupply, deltaAmount)
+            : getDeltaBaseAmount(circulatingSupply, deltaAmount);
     }
 
     // TOKEN/USD -> TOKEN is base, USD is quote
@@ -101,30 +121,31 @@ contract TMMarket is PricePoints, ImmutableContract, ITMMarket {
         if (recipient == address(0)) revert Market__InvalidRecipient();
         if (deltaAmount == 0) revert Market__ZeroAmount();
 
-        uint256 circulatingSupply = _circulatingSupply;
+        (uint256 baseReserve, uint256 quoteReserve) = _getReserves();
+        uint256 circulatingSupply = _totalSupply() - baseReserve;
 
         (deltaBaseAmount, deltaQuoteAmount) = (deltaAmount > 0) == fillBid
             ? getDeltaQuoteAmount(circulatingSupply, deltaAmount)
             : getDeltaBaseAmount(circulatingSupply, deltaAmount);
-
-        _circulatingSupply = uint128(Math.addDelta(circulatingSupply, -deltaBaseAmount));
 
         (uint256 toSend, uint256 toReceive, IERC20 tokenToSend, IERC20 tokenToReceive) = fillBid
             ? (Math.abs(deltaQuoteAmount), Math.abs(deltaBaseAmount), IERC20(_quoteToken()), IERC20(_baseToken()))
             : (Math.abs(deltaBaseAmount), Math.abs(deltaQuoteAmount), IERC20(_baseToken()), IERC20(_quoteToken()));
 
         if (toSend > 0) IERC20(tokenToSend).safeTransfer(recipient, toSend);
-
-        uint256 balance = tokenToReceive.balanceOf(address(this));
-
         if (
-            ITokenMillCallback(msg.sender).tokenMillSwapCallback(deltaBaseAmount, deltaQuoteAmount, data)
-                != ITokenMillCallback.tokenMillSwapCallback.selector
+            data.length > 0
+                && ITokenMillCallback(msg.sender).tokenMillSwapCallback(deltaBaseAmount, deltaQuoteAmount, data)
+                    != ITokenMillCallback.tokenMillSwapCallback.selector
         ) {
             revert Market__InvalidSwapCallback();
         }
 
-        if (balance + toReceive < tokenToReceive.balanceOf(address(this))) revert Market__InsufficientAmount();
+        uint256 balance = tokenToReceive.balanceOf(address(this));
+
+        fillBid
+            ? _updateReservesOnFillBid(baseReserve, quoteReserve, toSend, toReceive, balance)
+            : _updateReservesOnFillAsk(baseReserve, quoteReserve, toSend, toReceive, balance);
 
         emit Swap(msg.sender, recipient, deltaBaseAmount, deltaQuoteAmount);
     }
@@ -141,7 +162,8 @@ contract TMMarket is PricePoints, ImmutableContract, ITMMarket {
 
         IERC20 quoteToken = IERC20(_quoteToken());
 
-        (uint256 pendingProtocolFees, uint256 pendingCreatorFees) = _getPendingFees(factory, quoteToken);
+        (uint256 quoteBalance, uint256 pendingProtocolFees, uint256 pendingCreatorFees) =
+            _getPendingFees(factory, quoteToken);
 
         uint256 protocolTotalFees = _protocolTotalFees + pendingProtocolFees;
         uint256 creatorTotalFees = _creatorTotalFees + pendingCreatorFees;
@@ -160,6 +182,8 @@ contract TMMarket is PricePoints, ImmutableContract, ITMMarket {
         }
 
         if (fees > 0) {
+            _quoteReserve = quoteBalance - fees;
+
             quoteToken.safeTransfer(recipient, fees);
 
             emit FeesClaimed(caller, recipient, fees);
@@ -169,12 +193,12 @@ contract TMMarket is PricePoints, ImmutableContract, ITMMarket {
     function _getPendingFees(ITMFactory factory, IERC20 quoteToken)
         internal
         view
-        returns (uint256 protocolFees, uint256 creatorFees)
+        returns (uint256 quoteBalance, uint256 protocolFees, uint256 creatorFees)
     {
-        uint256 quoteBalance = quoteToken.balanceOf(address(this));
-        (, uint256 minQuoteAmount) = _getQuoteAmount(0, _circulatingSupply, false);
+        quoteBalance = quoteToken.balanceOf(address(this));
+        (, uint256 minQuoteAmount) = _getQuoteAmount(0, _totalSupply() - _baseReserve, false);
 
-        if (quoteBalance <= minQuoteAmount) return (0, 0);
+        if (quoteBalance <= minQuoteAmount) return (quoteBalance, 0, 0);
 
         uint256 totalFees = quoteBalance - minQuoteAmount;
         uint256 protocolShare = factory.getProtocolShareOf(address(this));
@@ -182,7 +206,37 @@ contract TMMarket is PricePoints, ImmutableContract, ITMMarket {
         uint256 pendingProtocolFees = totalFees * protocolShare / 1e18;
         uint256 pendingCreatorFees = totalFees - pendingProtocolFees;
 
-        return (pendingProtocolFees, pendingCreatorFees);
+        return (quoteBalance, pendingProtocolFees, pendingCreatorFees);
+    }
+
+    function _getReserves() internal view returns (uint256, uint256) {
+        return (_baseReserve, _quoteReserve);
+    }
+
+    function _updateReservesOnFillBid(
+        uint256 baseReserve,
+        uint256 quoteReserve,
+        uint256 toSend,
+        uint256 toReceive,
+        uint256 baseBalance
+    ) internal {
+        if (baseReserve + toReceive > baseBalance) revert Market__InsufficientAmount();
+
+        _baseReserve = uint128(baseBalance);
+        _quoteReserve = quoteReserve - toSend;
+    }
+
+    function _updateReservesOnFillAsk(
+        uint256 baseReserve,
+        uint256 quoteReserve,
+        uint256 toSend,
+        uint256 toReceive,
+        uint256 quoteBalance
+    ) internal {
+        if (quoteReserve + toReceive > quoteBalance) revert Market__InsufficientAmount();
+
+        _baseReserve = uint128(baseReserve - toSend);
+        _quoteReserve = quoteBalance;
     }
 
     function _factory() internal pure returns (address) {
@@ -217,7 +271,7 @@ contract TMMarket is PricePoints, ImmutableContract, ITMMarket {
         return _getUint(108, 16);
     }
 
-    function _pricePoints(uint256 i, bool bid) internal pure override returns (uint256) {
-        return _getUint((bid ? 126 : 110) + i * 32, 128);
+    function _pricePoints(uint256 i, bool fillBid) internal pure override returns (uint256) {
+        return _getUint((fillBid ? 110 : 126) + i * 32, 128);
     }
 }
