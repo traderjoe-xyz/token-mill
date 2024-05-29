2ae4386e0dca2d765dab42d6281686e6589b7001
0x0Louis <louis@joepegs.com>
2024-05-29 17:42:11 +0200

    Refactor lib tests

--- test/libraries/PricePoints.t.sol
+++ test/libraries/PricePoints.t.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
 
-import "../src/TMMarket.sol";
-import "../src/libraries/ImmutableCreate.sol";
-import "../src/libraries/ImmutableHelper.sol";
+import "../../src/TMMarket.sol";
+import "../../src/libraries/ImmutableCreate.sol";
+import "../../src/libraries/ImmutableHelper.sol";
 
 contract PricePointsTest is Test {
     MarketTestContract _market;
